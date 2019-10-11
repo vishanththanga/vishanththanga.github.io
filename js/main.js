7c9ac3aafb07ba0791e8ca2f10c601e48cb8c8ac
Vishanth <vish646464@gmail.com>
2019-10-11 17:38:17 -0400

    dark mode

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-import commentBox from "commentbox.io";
+var toggle = document.getElementById("dark-mode-toggle");
+var darkTheme = document.getElementById("dark-mode-theme");
 
-commentBox("5644966768410624-proj");
+toggle.addEventListener("click", () => {
+  if (toggle.className === "fal fa-moon-o") {
+    setTheme("dark");
+  } else if (toggle.className === "fal fa-sun-o") {
+    setTheme("light");
+  }
+});
+
+function setTheme(mode) {
+  if (mode === "dark") {
+    darkTheme.disabled = false;
+    toggle.className = "fal fa-sun-o";
+  } else if (mode === "light") {
+    darkTheme.disabled = true;
+    toggle.className = "fal fa-moon-o";
+  }
+}
